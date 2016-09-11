e6dfbdcdb33da28b3f1a0a085c99516eeb246a00
Thang Le-Surface 4 <letrthang@hotmail.com>
2016-09-11 14:01:17 +0800

    delete bins

--- MasterMind_Game_C_plus/Debug/src/CodePeg.d
+++
@@ -1,7 +0,0 @@
-src/CodePeg.d: ../src/CodePeg.cpp \
- /home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/Common.h \
- /home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/CodePeg.h
-
-/home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/Common.h:
-
-/home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/CodePeg.h:
--- MasterMind_Game_C_plus/Debug/src/KeyPeg.d
+++
@@ -1,7 +0,0 @@
-src/KeyPeg.d: ../src/KeyPeg.cpp \
- /home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/Common.h \
- /home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/KeyPeg.h
-
-/home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/Common.h:
-
-/home/thangle/eclipse_workspace/MasterMind_ThangLe/inc/KeyPeg.h:
